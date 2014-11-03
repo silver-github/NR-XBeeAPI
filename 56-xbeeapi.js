855ad24fc7f31c6ff60aa802b9e7b15b8df494ed
Nick Lucent <nick@allobaby.net>
2014-11-03 02:03:03 -0500

    Receiving messages works, sending not implemented yet

---
+++ 56-xbeeapi.js
@@ -0,0 +1,241 @@
+/**
+* Heavily based on core\io\25-serial.js
+*
+* Copyright 2013,2014 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+module.exports = function(RED) {
+    "use strict";
+    var settings = RED.settings;
+    var events = require("events");
+    var util = require("util");
+    var serialp = require("serialport");
+    var bufMaxSize = 32768;  // Max serial buffer size, for inputs...
+    var xbeeApi = require("xbee-api");
+
+    // 25-serial.js comment
+    // TODO: 'xbeePool' should be encapsulated in SerialPortNode
+
+    function XbeeAPINode(n) {
+        RED.nodes.createNode(this,n);
+        this.serialport = n.serialport;
+        //this.newline = n.newline;
+        //this.addchar = n.addchar || "false";
+        this.serialbaud = parseInt(n.serialbaud) || 9600;
+        this.databits = parseInt(n.databits) || 8;
+        this.parity = n.parity || "none";
+        this.stopbits = parseInt(n.stopbits) || 1;
+        this.bin = n.bin || "true";
+        //this.out = n.out || "char";
+    }
+    RED.nodes.registerType("xbeeapi",XbeeAPINode);
+
+    function XbeeAPIOutNode(n) {
+        RED.nodes.createNode(this,n);
+        this.serial = n.serial;
+        this.serialConfig = RED.nodes.getNode(this.serial);
+        this.xbee = n.xbee;
+
+        if (this.serialConfig) {
+            var node = this;
+            node.port = xbeePool.get(this.serialConfig.serialport,
+                this.serialConfig.serialbaud
+                //,
+                // this.serialConfig.databits,
+                // this.serialConfig.parity,
+                // this.serialConfig.stopbits,
+                // this.serialConfig.newline);
+            );
+            // node.addCh = "";
+            // if (node.serialConfig.addchar == "true") {
+            //     node.addCh = this.serialConfig.newline.replace("\\n","\n").replace("\\r","\r").replace("\\t","\t").replace("\\e","\e").replace("\\f","\f").replace("\\0","\0");
+            // }
+            node.on("input",function(msg) {
+                var payload = msg.payload;
+                if (!Buffer.isBuffer(payload)) {
+                    // not buffer so buildFrame
+                    payload = this.xbee.buildFrame(payload);
+                    // if (typeof payload === "object") {
+                    //     payload = JSON.stringify(payload);
+                    // } else {
+                    //     payload = payload.toString();
+                    // }
+                    //payload += node.addCh;
+                }
+
+                // else if (node.addCh !== "") {
+                //     payload = Buffer.concat([payload,new Buffer(node.addCh)]);
+                // }
+                node.port.write(payload,function(err,res) {
+                    if (err) {
+                        node.error(err);
+                    }
+                });
+            });
+            node.port.on('ready', function() {
+                node.status({fill:"green",shape:"dot",text:"connected"});
+            });
+            node.port.on('closed', function() {
+                node.status({fill:"red",shape:"ring",text:"not connected"});
+            });
+        } else {
+            this.error("missing serial config");
+        }
+
+        this.on("close", function(done) {
+            if (this.serialConfig) {
+                xbeePool.close(this.serialConfig.serialport,done);
+            } else {
+                done();
+            }
+        });
+    }
+    RED.nodes.registerType("xbeeapi out",XbeeAPIOutNode);
+
+
+    function XbeeAPIInNode(n) {
+        RED.nodes.createNode(this,n);
+        this.serial = n.serial;
+        this.serialConfig = RED.nodes.getNode(this.serial);
+        this.xbee = n.xbee;
+
+        if (this.serialConfig) {
+            var node = this;
+            node.tout = null;
+            var buf;
+            if (node.serialConfig.out != "count") { buf = new Buffer(bufMaxSize); }
+            else { buf = new Buffer(Number(node.serialConfig.newline)); }
+            var i = 0;
+            node.status({fill:"grey",shape:"dot",text:"unknown"});
+            node.xbee = xbeePool.get(this.serialConfig.serialport,
+                this.serialConfig.serialbaud
+            );
+
+            this.xbee.on('data', function(msg) {
+                console.log("received xbee message: " + msg)
+                 node.send({"payload": msg});
+            });
+
+        } else {
+            this.error("missing serial config");
+        }
+
+        this.on("close", function(done) {
+            if (this.serialConfig) {
+                xbeePool.close(this.serialConfig.serialport,done);
+            } else {
+                done();
+            }
+        });
+    }
+    RED.nodes.registerType("xbeeapi in",XbeeAPIInNode);
+
+
+    var xbeePool = function() {
+        var connections = {};
+        return {
+            get:function(port,baud,callback) {
+                var id = port;
+                if (!connections[id]) {
+                    connections[id] = function() {
+                        var obj = {
+                            _emitter: new events.EventEmitter(),
+                            serial: null,
+                            _closing: false,
+                            tout: null,
+                            xbee: null,
+                            on: function(a,b) { this._emitter.on(a,b); },
+                            close: function(cb) { this.serial.close(cb); },
+                            write: function(m,cb) { this.serial.write(m,cb); },
+                        }
+                        var setupXbee = function() {
+                                obj.xbee = new xbeeApi.XBeeAPI({ 
+                                    api_mode:2,
+                                    module: "ZigBee"
+                                });
+
+                                obj.xbee.on('frame_object', function (frame) {
+                                    console.log("Xbee data received: " + JSON.stringify(frame));
+                                    obj._emitter.emit('data',frame);
+                                });
+
+                                obj.serial = new serialp.SerialPort(port,{
+                                    baudrate: baud,
+                                    parser: obj.xbee.rawParser()
+                                },true, function(err, results) { if (err) { obj.serial.emit('error',err); } });
+
+                            obj.serial.on('error', function(err) {
+                                util.log("[xbee] serial port "+port+" error "+err);
+                                obj._emitter.emit('closed');
+                                obj.tout = setTimeout(function() {
+                                    setupXbee();
+                                }, settings.serialReconnectTime);
+                            });
+                            obj.serial.on('close', function() {
+                                if (!obj._closing) {
+                                    util.log("[xbee] serial port "+port+" closed unexpectedly");
+                                    obj._emitter.emit('closed');
+                                    obj.tout = setTimeout(function() {
+                                        setupXbee();
+                                    }, settings.serialReconnectTime);
+                                }
+                            });
+                            obj.serial.on('open',function() {
+                                util.log("[xbee] serial port "+port+" opened at "+baud+" baud ");
+                                if (obj.tout) { clearTimeout(obj.tout); }
+                                //obj.serial.flush();
+                                obj._emitter.emit('ready');
+                            });
+
+                            obj.serial.on("disconnect",function() {
+                                util.log("[xbee] serial port "+port+" disconnected");
+                            });
+                        }
+                        setupXbee();
+                        return obj;
+                    }();
+                }
+                return connections[id];
+            },
+            close: function(port,done) {
+                if (connections[port]) {
+                    if (connections[port].tout != null) {
+                        clearTimeout(connections[port].tout);
+                    }
+                    connections[port]._closing = true;
+                    try {
+                        connections[port].close(function() {
+                            util.log("[xbee] serial port closed");
+                            done();
+                        });
+                    }
+                    catch(err) { }
+                    delete connections[port];
+                } else {
+                    done();
+                }
+            }
+        }
+    }();
+
+    RED.httpAdmin.get("/xbeeports",function(req,res) {
+        serialp.list(function (err, ports) {
+            //console.log(JSON.stringify(ports));
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(JSON.stringify(ports));
+            res.end();
+        });
+    });
+}
--- README.md
+++ README.md
@@ -1,14 +1,12 @@
-NR-XBee
+NR-XBeeAPI
 =======
 
-Node-RED module to support use of XBee wireless modules.
-Right now this is an exact fork of freakent's [NR-XBee](https://github.com/freakent/NR-XBee).
-However, instead of [svd-xbee](https://github.com/jouz/svd-xbee), I'd like to use [xbee-api](https://github.com/jouz/svd-xbee) in the future.
+Node-RED module to support use of XBee wireless modules using xbee-api.
+Based on Node-Red serial.js
 
 Pre-requesites
 --------------
 1. Node-RED's serial module must be enabled
 1. Install xbee-api from npm
-
     $ npm install xbee-api
 
